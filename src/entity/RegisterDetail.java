d90a3964158b9f2423bb6ab4055e023d7a83160a
pramodsaranga <pramodsaranga31@gmail.com>
2022-10-04 19:58:28 +0530

    Content Updated

--- src/entity/RegisterDetail.java
+++ src/entity/RegisterDetail.java
@@ -1,4 +1,50 @@
 package entity;
 
-public class RegisterDetail {
+import javax.persistence.*;
+
+@Entity
+public class RegisterDetail implements SuperEntity{
+    @Id
+    private String registerId;
+
+    @ManyToOne(fetch = FetchType.EAGER,cascade = CascadeType.ALL)
+    @JoinColumn(name = "programId")
+    private Program programId;
+
+    @ManyToOne
+    @JoinColumn(name = "sid")
+    private Student sid;
+
+    public RegisterDetail(String registerId, Program programId, Student sid) {
+        this.setRegisterId(registerId);
+        this.setProgramId(programId);
+        this.setSid(sid);
+    }
+
+    public RegisterDetail() {
+    }
+
+    public String getRegisterId() {
+        return registerId;
+    }
+
+    public void setRegisterId(String registerId) {
+        this.registerId = registerId;
+    }
+
+    public Program getProgramId() {
+        return programId;
+    }
+
+    public void setProgramId(Program programId) {
+        this.programId = programId;
+    }
+
+    public Student getSid() {
+        return sid;
+    }
+
+    public void setSid(Student sid) {
+        this.sid = sid;
+    }
 }
