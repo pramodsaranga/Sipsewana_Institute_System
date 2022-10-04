d2681a7a7f3e6933698ca0ddeb4383bf2ed096da
pramodsaranga <pramodsaranga31@gmail.com>
2022-10-04 08:35:46 +0530

    Updated Context

--- src/controller/AdminFormController.java
+++ src/controller/AdminFormController.java
@@ -1,26 +1,51 @@
 package controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
 
 public class AdminFormController {
     public AnchorPane adminContext;
 
-    public void openLoginFormOnAction(ActionEvent actionEvent) {
+
+    public void openHomeFormOnAction(ActionEvent actionEvent) throws IOException {
+        URL resource = getClass().getResource("../view/DashBoardForm.fxml");
+        Parent load = FXMLLoader.load(resource);
+        Stage window = (Stage) adminContext.getScene().getWindow();
+        window.setScene(new Scene(load));
     }
 
-    public void openHomeFormOnAction(ActionEvent actionEvent) {
+    public void openAboutFormOnAction(ActionEvent actionEvent) throws IOException {
+        URL resource = getClass().getResource("../view/AboutForm.fxml");
+        Parent load = FXMLLoader.load(resource);
+        Stage window = (Stage) adminContext.getScene().getWindow();
+        window.setScene(new Scene(load));
     }
 
-    public void openAboutFormOnAction(ActionEvent actionEvent) {
+    public void openStudentFormOnAction(ActionEvent actionEvent) throws IOException {
+        URL resource = getClass().getResource("../view/StudentForm.fxml");
+        Parent load = FXMLLoader.load(resource);
+        Stage window = (Stage) adminContext.getScene().getWindow();
+        window.setScene(new Scene(load));
     }
 
-    public void openStudentFormOnAction(ActionEvent actionEvent) {
+    public void openProgrammesFormOnAction(ActionEvent actionEvent) throws IOException {
+        URL resource = getClass().getResource("../view/ProgrammesForm.fxml");
+        Parent load = FXMLLoader.load(resource);
+        Stage window = (Stage) adminContext.getScene().getWindow();
+        window.setScene(new Scene(load));
     }
 
-    public void openProgrammesFormOnAction(ActionEvent actionEvent) {
-    }
-
-    public void openPaymentFormOnAction(ActionEvent actionEvent) {
+    public void openPaymentFormOnAction(ActionEvent actionEvent) throws IOException {
+        URL resource = getClass().getResource("../view/PaymentForm.fxml");
+        Parent load = FXMLLoader.load(resource);
+        Stage window = (Stage) adminContext.getScene().getWindow();
+        window.setScene(new Scene(load));
     }
 }
