93ce0c8ca3811889b13c2773ace42d3ff746bf30
pramodsaranga <pramodsaranga31@gmail.com>
2022-10-04 08:24:53 +0530

    Updated Context

---
+++ src/controller/AdminFormController.java
@@ -0,0 +1,26 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.layout.AnchorPane;
+
+public class AdminFormController {
+    public AnchorPane adminContext;
+
+    public void openLoginFormOnAction(ActionEvent actionEvent) {
+    }
+
+    public void openHomeFormOnAction(ActionEvent actionEvent) {
+    }
+
+    public void openAboutFormOnAction(ActionEvent actionEvent) {
+    }
+
+    public void openStudentFormOnAction(ActionEvent actionEvent) {
+    }
+
+    public void openProgrammesFormOnAction(ActionEvent actionEvent) {
+    }
+
+    public void openPaymentFormOnAction(ActionEvent actionEvent) {
+    }
+}
