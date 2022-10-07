484f3b0d26cc0abb5e865ee60f603bdaf2c6684f
pramodsaranga <pramodsaranga31@gmail.com>
2022-10-07 10:25:56 +0530

    Content Updated

--- src/controller/PaymentDetailsFormController.java
+++ src/controller/PaymentDetailsFormController.java
@@ -1,5 +1,12 @@
 package controller;
 
+import bo.BoFactory;
+import bo.custom.ProgramBO;
+import bo.custom.RegisterBO;
+import bo.custom.StudentBO;
+import entity.Registration;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -7,26 +14,70 @@ import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import view.tm.RegisterTM;
 
 import java.io.IOException;
 import java.net.URL;
+import java.sql.SQLException;
+import java.util.List;
 
 public class PaymentDetailsFormController {
+
+    private final RegisterBO registerBO = (RegisterBO) BoFactory.getBOFactory().getBO(BoFactory.BoTypes.REGISTER);
+    private final StudentBO studentBO = (StudentBO) BoFactory.getBOFactory().getBO(BoFactory.BoTypes.STUDENT);
+    private final ProgramBO programBO = (ProgramBO) BoFactory.getBOFactory().getBO(BoFactory.BoTypes.PROGRAM);
+
     public AnchorPane paymentDetailsContext;
-    public TableView tblList;
     public TableColumn colRegisterId;
     public TableColumn colStudentId;
-    public TableColumn colProgramId;
     public TableColumn colStudentName;
-    public TableColumn colProgramName;
     public TableColumn colPayment;
 
     public Label txtDate;
 
     public Label txtTime;
+    public TableView tblRegisterList;
+    public TableColumn colAddress;
 
+    public void initialize() {
+
+        colAddress.setCellValueFactory(new PropertyValueFactory<>("address"));
+        colRegisterId.setCellValueFactory(new PropertyValueFactory<>("registerId"));
+        colStudentId.setCellValueFactory(new PropertyValueFactory<>("studentId"));
+        colStudentName.setCellValueFactory(new PropertyValueFactory<>("studentName"));
+        colPayment.setCellValueFactory(new PropertyValueFactory<>("payment"));
+
+        colAddress.setStyle("-fx-border-color: #58B19F;-fx-table-cell-border-color:#58B19F;");
+        colRegisterId.setStyle("-fx-border-color: #58B19F;-fx-table-cell-border-color:#58B19F;");
+        colPayment.setStyle("-fx-border-color: #58B19F;-fx-table-cell-border-color:#58B19F;");
+        colStudentId.setStyle("-fx-border-color: #58B19F;-fx-table-cell-border-color:#58B19F;");
+        colStudentName.setStyle("-fx-border-color: #58B19F;-fx-table-cell-border-color:#58B19F;");
+        try {
+            loadAllDetails();
+        } catch (SQLException | ClassNotFoundException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    private void loadAllDetails() throws SQLException, ClassNotFoundException {
+        tblRegisterList.getItems().clear();
+        ObservableList<RegisterTM> observableList= FXCollections.observableArrayList();
+        List<Registration> allRegisters = registerBO.getAllForAll();
+
+        for (Registration temp : allRegisters){
+            observableList.add(new RegisterTM(
+                    temp.getRegisterId(),
+                    temp.getStudent().getStudentId(),
+                    temp.getStudent().getStudentName(),
+                    temp.getStudent().getAddress(),
+                    temp.getPayment()
+            ));
+        }
+        tblRegisterList.setItems(observableList);
+    }
     public void openAdminFormOnAction(ActionEvent actionEvent) throws IOException {
         URL resource = getClass().getResource("../view/AdminForm.fxml");
         Parent load = FXMLLoader.load(resource);
