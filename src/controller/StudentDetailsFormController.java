586f34a0b08dfd2c9d2c3e54bc340d6da0931a19
pramodsaranga <pramodsaranga31@gmail.com>
2022-10-07 09:46:55 +0530

    Content Updated

--- src/controller/StudentDetailsFormController.java
+++ src/controller/StudentDetailsFormController.java
@@ -1,5 +1,9 @@
 package controller;
 
+import bo.BoFactory;
+import bo.custom.StudentBO;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -7,19 +11,25 @@ import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import view.tm.StudentTM;
 
 import java.io.IOException;
 import java.net.URL;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class StudentDetailsFormController {
+
+    private final StudentBO studentBO = (StudentBO) BoFactory.getBOFactory().getBO(BoFactory.BoTypes.STUDENT);
     public AnchorPane studentDetailsContext;
     public TableView tblStudent;
     public TableColumn colStudentId;
     public TableColumn colFullName;
     public TableColumn colAddress;
-    public TableColumn coBirthday;
+    public TableColumn colBirthday;
     public TableColumn colAge;
     public TableColumn colGender;
     public TableColumn colPhoneNumber;
@@ -29,6 +39,36 @@ public class StudentDetailsFormController {
 
     public Label txtTime;
 
+    public void initialize() throws SQLException, ClassNotFoundException {
+        setItemsToTable(studentBO.getAll());
+
+        colStudentId.setCellValueFactory(new PropertyValueFactory<>("studentId"));
+        colFullName.setCellValueFactory(new PropertyValueFactory<>("studentName"));
+        colAddress.setCellValueFactory(new PropertyValueFactory<>("address"));
+        colBirthday.setCellValueFactory(new PropertyValueFactory<>("birthday"));
+        colAge.setCellValueFactory(new PropertyValueFactory<>("age"));
+        colGender.setCellValueFactory(new PropertyValueFactory<>("gender"));
+        colPhoneNumber.setCellValueFactory(new PropertyValueFactory<>("phoneNumber"));
+        colEducation.setCellValueFactory(new PropertyValueFactory<>("education"));
+
+        colStudentId.setStyle("-fx-border-color:  #58B19F;-fx-table-cell-border-color: #58B19F;");
+        colFullName.setStyle("-fx-border-color:  #58B19F;-fx-table-cell-border-color: #58B19F;");
+        colAddress.setStyle("-fx-border-color:  #58B19F;-fx-table-cell-border-color: #58B19F;");
+        colBirthday.setStyle("-fx-border-color:  #58B19F;-fx-table-cell-border-color: #58B19F;");
+        colAge.setStyle("-fx-border-color:  #58B19F;-fx-table-cell-border-color: #58B19F;");
+        colGender.setStyle("-fx-border-color:  #58B19F;-fx-table-cell-border-color: #58B19F;");
+        colPhoneNumber.setStyle("-fx-border-color:  #58B19F;-fx-table-cell-border-color: #58B19F;");
+        colEducation.setStyle("-fx-border-color:  #58B19F;-fx-table-cell-border-color: #58B19F;");
+    }
+
+    private void setItemsToTable(ArrayList<StudentTM> student) {
+        ObservableList<StudentTM> obList = FXCollections.observableArrayList();
+        student.forEach(e -> {
+            obList.add(
+                    new StudentTM(e.getStudentId(),e.getStudentName(),e.getAddress(),e.getBirthday(),e.getAge(),e.getGender(),e.getPhoneNumber(),e.getEducation()));
+        });
+        tblStudent.setItems(obList);
+    }
     public void openAdminFormOnAction(ActionEvent actionEvent) throws IOException {
         URL resource = getClass().getResource("../view/AdminForm.fxml");
         Parent load = FXMLLoader.load(resource);
