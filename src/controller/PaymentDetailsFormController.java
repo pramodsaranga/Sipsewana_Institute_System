9b9378600aa775eaa40db71c4cb3996c9f099d7b
pramodsaranga <pramodsaranga31@gmail.com>
2022-10-07 09:20:25 +0530

    Content Updated

--- src/controller/PaymentDetailsFormController.java
+++ src/controller/PaymentDetailsFormController.java
@@ -1,4 +1,25 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.AnchorPane;
+
 public class PaymentDetailsFormController {
+    public AnchorPane paymentDetailsContext;
+    public TableView tblList;
+    public TableColumn colRegisterId;
+    public TableColumn colStudentId;
+    public TableColumn colProgramId;
+    public TableColumn colStudentName;
+    public TableColumn colProgramName;
+    public TableColumn colPayment;
+
+    public Label txtDate;
+
+    public Label txtTime;
+
+    public void openAdminFormOnAction(ActionEvent actionEvent) {
+    }
 }
--- src/controller/ProgrammeDetailsFormController.java
+++ src/controller/ProgrammeDetailsFormController.java
@@ -1,4 +1,21 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.AnchorPane;
+
 public class ProgrammeDetailsFormController {
+    public AnchorPane programDetailsContext;
+    public TableView tblProgram;
+    public TableColumn colProgramId;
+    public TableColumn colProgramName;
+    public TableColumn colDuration;
+    public TableColumn colFee;
+    public Label txtDate;
+    public Label txtTime;
+
+    public void openAdminFormOnAction(ActionEvent actionEvent) {
+    }
 }
